c44f648ffd564e2c8b0d017ff45f83ace050da21
Scott Jones <scottdj92@gmail.com>
2018-07-21 08:15:31 -0400

    initial coverage of HOC test

--- __tests__/WithArtemis.spec.tsx
+++ __tests__/WithArtemis.spec.tsx
@@ -1,1 +1,16 @@
 import React from "react";
+
+describe("withArtemis", () => {
+    it("Calls getIntialProps if it is available", () => {
+        console.log("test");
+    });
+
+    it("Rewinds the next.js Head", () => {
+        // check to see if the function is called
+        console.log("head");
+    });
+
+    it("Constructs an instance of the wrapped component with spreaded props", () => {
+        console.log("component");
+    });
+});
